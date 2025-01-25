3e083e4ecb367b414ada26cd55f2ce01682c6a69
Luiz Ricardo <luu.izz@hotmail.com>
2025-01-25 10:25:21 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+    <h1>Começo do projeto</h1>
+  );
+}
---
+++ src/app/sobre-nos/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SobreNos() {
+  return (
+    <div>Página de Sobre nós</div>
+  )
+}
