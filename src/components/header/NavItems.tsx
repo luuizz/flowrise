1efa3a8e0c57091c0009f3fa4b2620906ef4b329
Luiz Ricardo <luu.izz@hotmail.com>
2025-02-15 15:29:17 -0300

    feat: Criado componente de Header e navegação

---
+++ src/components/header/NavItems.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { navItems } from '@/utils/data';
+import Link from 'next/link';
+
+
+
+export default function NavItems() {
+  return (
+    <nav>
+      <ul className='flex items-center gap-8'>
+        {navItems.map((item) => (
+          <li key={item.path}>
+            <Link className='font-display text-base/normal text-slate-700 transition-colors hover:text-slate-900' href={item.path}>{item.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+import Bounded from "../Bounded";
+import LogoFlowRise from "../assets/logo";
+import NavItems from "./NavItems";
+
 export default function Header() {
   return (
-    <header>
-      Header
+    <header className="py-7">
+      <Bounded className="flex items-center justify-between">
+        <Link title="Ir para página inicial" href="/">
+          <LogoFlowRise />
+        </Link>
+        <NavItems />
+      </Bounded>
     </header>
   )
 }
