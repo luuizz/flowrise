23acae124c223a7b6a9dcf45b70c057ce6fc4b10
Luiz Ricardo <luu.izz@hotmail.com>
2025-02-15 15:28:51 -0300

    feat: adicionado novos dados

---
+++ src/utils/data.ts
@@ -0,0 +1,14 @@
+export const navItems = [
+  {
+    path: "/funcionalidades",
+    label: "Funcionalidades",
+  },
+  {
+    path: "/quem-somos",
+    label: "Quem somos",
+  },
+  {
+    path: "/contato",
+    label: "Contato",
+  },
+]
