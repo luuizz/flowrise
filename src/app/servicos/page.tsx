0a3040eb26543cdad00d7ef48c4d0b0f118c7cf8
Luiz Ricardo <luu.izz@hotmail.com>
2025-02-15 15:29:54 -0300

    fix: alterado rotas

--- src/app/servicos/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Servicos() {
-  return (
-    <div>Página de servicos</div>
-  )
-}
--- src/app/sobre-nos/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function SobreNos() {
-  return (
-    <div>Página de Sobre nós</div>
-  )
-}
