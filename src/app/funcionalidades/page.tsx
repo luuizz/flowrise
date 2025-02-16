556d66a87f9abf4aba45932f874bac79f33e9822
Luiz Ricardo <luu.izz@hotmail.com>
2025-02-15 23:45:34 -0300

    feat: criado pagina de funcionalidades

--- src/app/funcionalidades/page.tsx
+++ src/app/funcionalidades/page.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import CallToAction from '@/components/CallToAction'
+import SectionHeroFuncionalidades from '@/components/Funcionalidades/SectionHeroFuncionalidades'
+import SectionFuncionalidades from '@/components/Home/SectionFuncionalidades'
+import SectionHarmonize from '@/components/Home/SectionHarmonize'
+import SectionTestimonials from '@/components/Home/SectionTestimonial'
 
 export default function Servicos() {
   return (
-    <div>Página de servicos</div>
+    <>
+      <SectionHeroFuncionalidades />
+      <SectionFuncionalidades />
+      <SectionHarmonize />
+      <SectionTestimonials />
+      <CallToAction />
+    </>
   )
 }
