c3551a112299439c38c5a6421568dc3db3ae335e
Luiz Ricardo <luu.izz@hotmail.com>
2025-02-15 21:09:29 -0300

    fix: corrigido tamanho de fonte

--- src/components/Home/SectionTestimonial/index.tsx
+++ src/components/Home/SectionTestimonial/index.tsx
@@ -7,7 +7,7 @@ export default function SectionTestimonials() {
   return (
     <section className='py-24'>
       <Bounded>
-        <h2 className='text-slate-700 font-body text-5xl/tight font-bold mb-9 text-center'>O que nossos clientes dizem</h2>
+        <h2 className='text-slate-700 font-body text-4xl/tight font-bold mb-9 text-center'>O que nossos clientes dizem</h2>
 
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
           {dataTestimonials.map((testimonial, index) => (  
