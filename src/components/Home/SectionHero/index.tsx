ecf052b3b43989329bff98aebd9bff02d8e0f4a9
Luiz Ricardo <luu.izz@hotmail.com>
2025-02-16 11:39:43 -0300

    feat: responsivo da Hero Section Home

--- src/components/Home/SectionHero/index.tsx
+++ src/components/Home/SectionHero/index.tsx
@@ -5,13 +5,13 @@ import Button from '@/components/Button'
 
 export default function SectionHero() {
   return (
-    <section className='py-20'>
+    <section className='py-10 md:py-16 lg:py-20'>
       <Bounded className='flex justify-center'>
         <main className='w-full max-w-[62.625rem]'>
-          <div className='text-center mb-28'>
-            <h1 className='text-7xl/tight font-body text-slate-700 font-bold mb-2'>Produtividade que se adapta à sua vida, não o contrário.</h1>
-            <p className='text-2xl/normal font-display text-slate-600 w-[28.5rem] mx-auto'>Descubra seu fluxo ideal: organize tarefas, hábitos e energia no seu ritmo.</p>
-            <Button className='mt-8' href="/contato">Experimente grátis</Button>
+          <div className='text-center mb-16 md:mb-20 lg:mb-28'>
+            <h1 className='text-balance text-4xl/short md:text-5xl xl:text-7xl font-body text-slate-700 font-bold mb-2'>Produtividade que se adapta à sua vida, não o contrário.</h1>
+            <p className='text-balance text-xl/base md:text-2xl font-display text-slate-600 w-[28.5rem] mx-auto'>Descubra seu fluxo ideal: organize tarefas, hábitos e energia no seu ritmo.</p>
+            <Button className='mt-6 md:mt-8' href="/contato">Experimente grátis</Button>
           </div>
 
           <Image className='mx-auto' alt='Dashboard da Flowrise' src="/dashboard.png" width={936} height={665} />
